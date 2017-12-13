49fa5f607de2d5dd3cbabbad9481dc1914001dc6
tony <by.zhu@live.cn>
2017-12-13 20:56:34 +0800

    主要是travis

--- tonyirl-passport/src/main/java/me/tony/tonyirl/passport/App.java
+++ tonyirl-passport/src/main/java/me/tony/tonyirl/passport/App.java
@@ -2,13 +2,17 @@ package me.tony.tonyirl.passport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author tony
  */
 @SpringBootApplication(scanBasePackages = "me.tony")
-@PropertySource("classpath:service-discovery.properties")
+@PropertySource(value = "classpath:service-discovery.properties", name = "remote")
+@EnableDiscoveryClient(autoRegister = false)
+@EnableFeignClients
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- tonyirl-passport/src/main/java/me/tony/tonyirl/passport/biz/impl/AccountBizImpl.java
+++ tonyirl-passport/src/main/java/me/tony/tonyirl/passport/biz/impl/AccountBizImpl.java
@@ -3,6 +3,7 @@ package me.tony.tonyirl.passport.biz.impl;
 import me.tony.tonyirl.account.service.model.Account;
 import me.tony.tonyirl.passport.biz.AccountBiz;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,9 @@ public class AccountBizImpl implements AccountBiz {
 
     @Autowired
     RestTemplate restTemplate;
+
+    @Value("${account-provider}")
+    private String name;
 
     @Override
     public Account getById(Long id) {
