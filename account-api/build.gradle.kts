0fd2e22051dee158cedf9d72b5ebf5caf11be1af
zhubaiyang <zhubaiyang@meituan.com>
2020-12-03 15:38:32 +0800

    优化项目结构

--- account-api/build.gradle.kts
+++ account-api/build.gradle.kts
@@ -8,5 +8,6 @@ dependencies {
 }
 
 tasks.withType<org.jruyi.gradle.thrift.plugin.CompileThrift> {
+    thriftExecutable = "/usr/local/opt/thrift/bin/thrift"
     generator("java", "private-members", "fullcamel", "option_type", "generated_annotations=undated")
 }
